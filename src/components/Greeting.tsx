9d860af6c741460fd54d42a8e2d6b22d3d79ac81
MeetParmar2302 <meet.parmar.2302@gmail.com>
2025-05-22 14:09:47 +0530

    Minor Fixes

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Greeting = () => {
   return (
-    <h1 className="text-4xl font-bold">Hi, I'm Meet Parmar 👋</h1>
+    <h1 className="text-4xl font-bold">Hi, I&apos;m Meet Parmar 👋</h1>
   )
 }
 
