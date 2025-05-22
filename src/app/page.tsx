c9307acd87685b04fffa1568611b28b270582459
MeetParmar2302 <meet.parmar.2302@gmail.com>
2025-05-22 14:50:03 +0530

    Added Mobile responsiveness in initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Greeting from "@/components/Greeting";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center bg-black text-white space-y-4">
+    <main className="min-h-screen flex flex-col items-center justify-center bg-black text-white px-4 text-center space-y-4">
       <Greeting />
-      <p className="text-xl">Portfolio Coming Soon 🚀</p>
+      <p className="text-xl sm:text-2xl md:text-3xl font-medium">Portfolio Coming Soon 🚀</p>
     </main>
   );
 }
--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Greeting = () => {
   return (
-    <h1 className="text-4xl font-bold">Hi, I&apos;m Meet Parmar 👋</h1>
+    <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold">Hi, I&apos;m Meet Parmar 👋</h1>
   )
 }
 
